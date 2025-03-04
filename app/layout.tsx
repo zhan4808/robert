d20fe158e3365a0df221ae39547128c38e49ed67
zhan4808 <zhan4808@purdue.edu>
2025-03-03 20:21:45 -0500

    speed analy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { ReactNode } from 'react';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Navbar from './components/Navbar';
 import { ThemeProvider } from './ThemeContext';
 

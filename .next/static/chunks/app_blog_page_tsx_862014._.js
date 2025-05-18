ed372445204475ad607659fdf1e58d679ee1fe3e
zhan4808 <zhan4808@purdue.edu>
2025-05-17 23:23:54 -0700

    bug?

---
+++ .next/static/chunks/app_blog_page_tsx_862014._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_blog_page_tsx_862014._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_blog_page_tsx_862014._.js",
+  "chunks": [
+    "static/chunks/app_5150e0._.js"
+  ],
+  "source": "dynamic"
+});
